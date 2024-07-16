dff874c94f8159c9fb0a4bf3decca32fb9574f3d
BilalMartin <m.bilalkhant@gmail.com>
2024-07-16 12:52:45 +0500

    updated: multi-bloc-provider

--- lib/multi_bloc_provider.dart
+++ lib/multi_bloc_provider.dart
@@ -1,6 +1,7 @@
 import 'package:eye_distance/logic/camera_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'logic/camera_distance_cubit.dart';
 
 class ProvideMultiBloc extends StatelessWidget {
   final Widget child;
@@ -12,6 +13,9 @@ class ProvideMultiBloc extends StatelessWidget {
     return MultiBlocProvider(providers: [
       BlocProvider(
         create: (context) => CameraCubit(),
+      ),
+      BlocProvider(
+        create: (context) => CameraDistanceCubit(),
       ),
     ], child: child);
   }
