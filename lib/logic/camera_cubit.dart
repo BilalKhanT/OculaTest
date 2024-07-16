1fc2569de56e083136f87c215609b9975ae24954
BilalMartin <m.bilalkhant@gmail.com>
2024-07-16 15:47:14 +0500

    updated: camera-cubit

--- lib/logic/camera_cubit.dart
+++ lib/logic/camera_cubit.dart
@@ -1,7 +1,9 @@
 import 'dart:developer';
-import 'package:bloc/bloc.dart';
 import 'package:camera/camera.dart';
 import 'package:eye_distance/logic/camera_state.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 class CameraCubit extends Cubit<CameraState> {
@@ -11,6 +13,7 @@ class CameraCubit extends Cubit<CameraState> {
   late List<CameraDescription> cameras;
   FaceDetector? faceDetector;
   List<Face>? facesList;
+  bool isProcessing = false;
 
   void disposeCamera() {
     camera.dispose();
@@ -31,19 +34,85 @@ class CameraCubit extends Cubit<CameraState> {
         imageFormatGroup: ImageFormatGroup.yuv420,
         enableAudio: false,
       );
-      await camera?.initialize();
-      await camera?.setFlashMode(FlashMode.off);
-      await camera?.setFocusMode(FocusMode.auto);
+      await camera.initialize();
+      await camera.setFlashMode(FlashMode.off);
+      await camera.setFocusMode(FocusMode.auto);
       faceDetector = FaceDetector(
           options: FaceDetectorOptions(
         performanceMode: FaceDetectorMode.accurate,
         enableLandmarks: true,
         enableClassification: true,
       ));
-      emit(CameraLoaded(camera!));
+      emit(CameraLoaded(camera));
+      camera.startImageStream((image) {
+        processImage(image, frontCamera.sensorOrientation);
+      });
     } catch (e) {
       log(e.toString());
       emit(const CameraError('Failed to load camera'));
+    }
+  }
+
+  Future<void> processImage(CameraImage img, int sensorOrientation) async {
+    if (isProcessing == true) {
+      return;
+    }
+    isProcessing = true;
+    final WriteBuffer allBytes = WriteBuffer();
+    for (Plane plane in img.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+    final imageSize = Size(img.width.toDouble(), img.height.toDouble());
+    final imageRotation = getRotation(sensorOrientation);
+    const inputImageFormat = InputImageFormat.yuv420;
+    final planeData = img.planes.first.bytesPerRow;
+    final inputImageData = InputImageMetadata(
+      size: imageSize,
+      rotation: imageRotation,
+      format: inputImageFormat,
+      bytesPerRow: planeData,
+    );
+    InputImage image =
+    InputImage.fromBytes(bytes: bytes, metadata: inputImageData);
+
+    final faces = await faceDetector?.processImage(image);
+
+    if (faces!.isNotEmpty) {
+      final face = faces.first;
+      final faceWidth = face.boundingBox.width;
+      calculateDistance(faceWidth, img.width,);
+    }
+    isProcessing = false;
+  }
+
+  InputImageRotation getRotation(int sensorOrientation) {
+    switch (sensorOrientation) {
+      case 0:
+        return InputImageRotation.rotation0deg;
+      case 90:
+        return InputImageRotation.rotation90deg;
+      case 180:
+        return InputImageRotation.rotation180deg;
+      case 270:
+        return InputImageRotation.rotation270deg;
+      default:
+        return InputImageRotation.rotation0deg;
+    }
+  }
+
+  void calculateDistance(double faceWidthPx, int imageWidthPx) {
+    const double sensorWidthMm = 6.4;
+    const double focalLengthMm = 4.15;
+    const double knownFaceWidthMm = 160.0;
+
+    final double faceWidthRatio = faceWidthPx / imageWidthPx;
+    final double distanceMm = (knownFaceWidthMm * focalLengthMm) / (faceWidthRatio * sensorWidthMm);
+
+    final double distanceMeters = distanceMm / 1000;
+
+    if (distanceMeters >= 0.4 && distanceMeters <= 0.6) {
+      emit(CameraSuccess());
     }
   }
 }
